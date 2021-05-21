d2dfdf2bfccae1e9fe3b50c3c1f142e79a395bbf
Federico Leonel Canesa <federicocanesa@gmail.com>
2021-05-21 13:02:01 -0300

    Se deja establecida la estructura básica del nav.js

---
+++ src/Components/Nav.js
@@ -0,0 +1,15 @@
+import React, { Fragment}  from 'react'
+
+const Nav = () => {
+    return(
+        <Fragment>
+            <nav className="nav">
+                <button>R2</button>
+                <button>R3</button>
+                <button>R4</button>
+            </nav>
+        </Fragment>
+    );
+
+}
+export default Nav
