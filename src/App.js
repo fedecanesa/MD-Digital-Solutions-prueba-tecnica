a743f03647191f9ab374c6214268d11fd2c93bfe
Federico Leonel Canesa <federicocanesa@gmail.com>
2021-05-21 12:58:34 -0300

    Se crean los componentes Header, Footer, Nav, Table(R2), y R3. Se importan a la aplicacion para trabajarlos individualmente.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Header from './Components/Header.js'
+import Nav from './Components/Nav';
+import Table from './Components/R2.js';
+import Web from './Components/R3.js'
+//import Mensaje from './Components/Mensaje.js';
+import Footer from './Components/Footer.js';
 
 function App() {
+
+  //Obtener la fecha para el footer
+  const date = new Date().getFullYear();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header 
+        title= 'Prueba Técnica desarrollo frontend | Federico Canesa'
+      />
+      <Nav/>
+      {/* <Mensaje /> */}
+      <Table />
+      <Web/>
+      <Footer 
+        date = {date}
+      /> 
     </div>
-  );
-}
+  )}
 
 export default App;
