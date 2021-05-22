9b8979e46c31faca216d442ba33e20c1e0a559d0
Federico Leonel Canesa <federicocanesa@gmail.com>
2021-05-22 13:56:41 -0300

    Se agrega enrutamiento con react router dom.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,16 @@
-import React, { Fragment}  from 'react'
+import React  from 'react'
+import { NavLink } from "react-router-dom";
+
+import '../css/index.css'
 
 const Nav = () => {
-    return(
-        <Fragment>
-            <nav className="nav">
-                <button>R2</button>
-                <button>R3</button>
-                <button>R4</button>
-            </nav>
-        </Fragment>
-    );
 
+    return(
+        <nav className="nav">
+            <NavLink exact to="/R2">R2</NavLink>
+            <NavLink exact to="/R3">R3</NavLink>
+            <NavLink exact to="/R4">R4</NavLink>
+        </nav>
+    );
 }
 export default Nav
