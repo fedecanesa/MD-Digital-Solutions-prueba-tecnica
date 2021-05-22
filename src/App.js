9fae8dfb9976be9ecd9e202ae132ddd0464c248a
Federico Leonel Canesa <federicocanesa@gmail.com>
2021-05-22 15:42:41 -0300

    Se agrega componente de bienvenida para ruta /

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './Components/Header'
-import Nav from './Components/Nav'
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Wellcome from './Components/Wellcome';
 import Table from './Components/R2/R2';
 import Web from './Components/R3/R3';
 import Users from './Components/R4/Users'; 
@@ -23,10 +24,12 @@ function App() {
       />
       <Nav />
         <Switch>
+          <Route exact path="/"><Wellcome /></Route>
           <Route exact path="/R2"><Table /></Route>
           <Route exact path="/R3"><Web /></Route>
           <Route exact path="/R4"><Users /></Route>
         </Switch>
+
       <Footer 
         date = {date}
       /> 
---
+++ src/Components/Wellcome.js
@@ -0,0 +1,15 @@
+import React  from 'react'
+
+import '../css/index.css'
+
+const Wellcome = () => {
+
+    return(
+        <div className="wellcome">
+            <h1 className="wellcame-h1">Bienvenido/a:</h1>
+            <p className="wellcome-introdution">Esta es la prueba técnica de Federico Canesa para aplicar al puesto de Desarrollador Web Trainee. Para ver los ejercicios, por favor, seleccione las opciones: R2 , R3 o R4.</p>
+            <h6 className="wellcome-h6">¡Muchas gracias por dejarme participar!</h6>
+        </div>
+    );
+}
+export default Wellcome;
